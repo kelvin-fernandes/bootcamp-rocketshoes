ef3c8ebfe3cc83ff53de22265d58358955def468
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-09-17 09:27:11 -0400

    Added Home product list style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes />
             <GlobalStyle />
             <Header />
+            <Routes />
         </BrowserRouter>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,86 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <h1>You're at Home</h1>;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis niceee</strong>
+                <span>R$1499,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis niceee</strong>
+                <span>R$1499,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis niceee</strong>
+                <span>R$1499,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis niceee</strong>
+                <span>R$1499,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x"
+                    alt="Tênis"
+                />
+                <strong>Tênis niceee</strong>
+                <span>R$1499,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }

9187094c0c07f3fbec120e81bbf9e57486902367
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-09-17 20:13:13 -0400

    Added redux config

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
+import store from './store';
 
 function App() {
     return (
-        <BrowserRouter>
-            <GlobalStyle />
-            <Header />
-            <Routes />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <GlobalStyle />
+                <Header />
+                <Routes />
+            </BrowserRouter>
+        </Provider>
     );
 }
 

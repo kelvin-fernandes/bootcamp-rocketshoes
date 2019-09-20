5aff3e93b3257ee8b3ccccd149e506b89ea544df
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-09-19 23:51:08 -0400

    Added reactotron config

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
         'react/jsx-filename-extension': [
             'warn', { extensions: ['.jsx', '.js'] }
         ],
-        'import/prefer-default-export': 'off'
+        'import/prefer-default-export': 'off',
+        'no-console': ['error', { allow: ['tron'] }]
     },
 };
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
